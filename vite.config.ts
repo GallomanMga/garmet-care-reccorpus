5f9741c146decae4135f6e8beaca3dc31c479074
Danilo Gallo <danilo.gallo1@gmail.com>
2023-01-25 13:57:05 -0300

    Websocket config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+   // https: true,
+    hmr: {
+      //host: "cuidados.altogiro.net""
+      host: "https://localhost",
+      protocol: "wss"
+    }
+  }
 })
+
+
+
+
